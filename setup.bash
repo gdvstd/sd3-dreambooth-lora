8f0f1fc3f0d02da317a177244b097816ceafc212
gdvstd <iblitzrod0324@gmail.com>
2024-06-27 17:00:44 +0000

    setup update

--- setup.bash
+++ setup.bash
@@ -1,6 +1,7 @@
 git clone https://github.com/huggingface/diffusers
 cd diffusers
 pip install -e .
-cd ../
 cd examples/dreambooth
 pip install -r requirements_sd3.txt
+pip install wandb bitsandbytes
+wandb login
